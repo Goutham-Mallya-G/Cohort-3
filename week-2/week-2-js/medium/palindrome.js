043e6326e2f222a28a7ec9d9157a4a096f8733cb
Goutham Mallya G <gouthammallya2@gmail.com>
2024-12-23 23:30:14 +0530

    Completed the assignment

--- week-2/week-2-js/medium/palindrome.js
+++ week-2/week-2-js/medium/palindrome.js
@@ -3,8 +3,24 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
-function isPalindrome(str) {
-  return true;
+function filtering(str){
+    let filter = ""
+    for(let char of str){
+      if((char >= 'a' && char<="z" ) || (char >= 'A' && char <= "Z") || (char >= "0" && char<= "9")){
+        filter += char.toLowerCase()
+      }
+    }
+    return filter
 }
 
+function isPalindrome(str) {
+  let filtered = filtering(str)
+  for (let i=0; i < filtered.length/2;i++){
+    if(filtered[i]!=filtered[filtered.length-i-1]){
+      return false;
+    }
+  }
+  return true
+}
+console.log(isPalindrome("Madam"))
 module.exports = isPalindrome;
